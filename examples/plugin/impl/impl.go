8baf3552b4b7e4b0bd1a50abbc7c6ba70238cb48
dezhiShen <1179551960@qq.com>
2021-12-30 15:19:59 +0800

    mod:修改代码顺序，补充注释

--- examples/plugin/impl/impl.go
+++ examples/plugin/impl/impl.go
@@ -13,7 +13,21 @@ func main() {
 	plugin.NewOnebotEventPluginBuilder().
 		//设置插件内容
 		Id("test").Name("测试插件").Description("这是测试插件").
-		//回调事件
+		//设置生命周期函数
+		Init(func(cli cli.OnebotCli) error {
+			d, e := cli.GetVersionInfo()
+			logrus.Info("============ version info ==========")
+			logrus.Info(d, e)
+			go loop(cli)
+			return nil
+		}).
+		BeforeExit(func(cli cli.OnebotCli) error {
+			d, e := cli.GetVersionInfo()
+			logrus.Info("========exit=======")
+			logrus.Info(d, e)
+			return nil
+		}).
+		//Onebot回调事件
 		MessageGroup(func(req *model.EventMessageGroup, cli cli.OnebotCli) error {
 			logrus.Infof("收到群组消息,UserId:[%v]", req.Sender.UserId)
 			return nil
@@ -30,19 +44,6 @@ func main() {
 		MetaLifecycle(func(req *model.EventMetaLifecycle, cli cli.OnebotCli) error {
 			logrus.Info("============status==========")
 			logrus.Info(cli.GetStatus())
-			return nil
-		}).
-		Init(func(cli cli.OnebotCli) error {
-			d, e := cli.GetVersionInfo()
-			logrus.Info("============ version info ==========")
-			logrus.Info(d, e)
-			go loop(cli)
-			return nil
-		}).
-		BeforeExit(func(cli cli.OnebotCli) error {
-			d, e := cli.GetVersionInfo()
-			logrus.Info("========exit=======")
-			logrus.Info(d, e)
 			return nil
 		}).
 		//构建插件
